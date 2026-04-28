f5ba8d696e9185d3e222a1f5cc17d329002f42d3
corybaird <31351628+corybaird@users.noreply.github.com>
2026-04-28 19:55:21 +0900

    REFACTOR module exports in package __init__ files

--- src/stats_transformer/__init__.py
+++ src/stats_transformer/__init__.py
@@ -1,9 +1,9 @@
 __version__ = "0.1.0"
 
-from stats_transformer.featurization import FeatureEngineer
-from stats_transformer.models import RegressionModel
-from stats_transformer.pipeline import Pipeline
-from stats_transformer.visualization import (
+from .featurization import FeatureEngineer
+from .models import RegressionModel
+from .pipeline import Pipeline
+from .visualization import (
     BaseVisualizer, DataVisualizer, ModelVisualizer, RegressionVisualizer
 )
 
--- src/stats_transformer/models/__init__.py
+++ src/stats_transformer/models/__init__.py
@@ -1,4 +1,4 @@
-from stats_transformer.models.base import ModelBase
+from src.stats_transformer.models.base import ModelBase
 from .regression.regression import RegressionModel
 from .regression.robust_ols import RobustOLSModel
 from .regression.panel import PanelRegressionModel
